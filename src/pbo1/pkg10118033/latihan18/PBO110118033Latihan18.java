41c8c713c88e8f12d9365a2ca9589845749c592a
IkrarAB <ikraranugrah26@gmail.com>
2019-10-09 12:35:04 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan18/PBO110118033Latihan18.java
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * @author 
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * bilangan kelipatan 3,5 dari 3,5 sampai 35
+ *  
+ */
+
+package pbo1.pkg10118033.latihan18;
+
+public class PBO110118033Latihan18 {
+    public static void main(String[] args) {
+        for(double i=3.5; i<= 35; i += 3.5){
+            System.out.println(i);
+        }
+        System.out.println("(Developed By : Ikrar AB)");
+    }
+}
